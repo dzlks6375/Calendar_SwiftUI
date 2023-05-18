ed5787d11bf5d004509e71719c2f42bd8afa2913
Siu Hwang <hsiwoo@icloud.com>
2023-05-18 18:41:03 +0900

    Initial Commit

---
+++ Calendar_SwiftUI/Calendar_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar_SwiftUIApp.swift
+//  Calendar_SwiftUI
+//
+//  Created by Siu on 2023/05/18.
+//
+
+import SwiftUI
+
+@main
+struct Calendar_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
